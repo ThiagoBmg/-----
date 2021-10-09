22c316d9825d094e184691c371125ec96bc91948
Thiago Braga Martins Gomes <thiago.gomes@pixeon.com>
2021-10-09 16:58:22 -0300

    init / adicionando opções do menu e de movimentações

---
+++ config/options.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* OPÇÕES TIPO DE MOVIMENTAÇÕES */
+#define TIPO_MOVIMENTACAO_N  5
+char TIPO_MOVIMENTACAO[TIPO_MOVIMENTACAO_N][20] = {"Moradia","Estudos", "Transporte", "Alimentação", "Trabalho"};
+
+/* OPÇÕES DO MENU */
+#define OPCOES_N  3
+char MENU_OPTIONS[OPCOES_N][20] = {"Novo Lancamento","Gerar Relatorio","Sobre Este App"};
