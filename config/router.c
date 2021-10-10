a99f452b2d6df206f366e87138b88e14c67d7b41
Thiago Braga Martins Gomes <thiago.gomes@pixeon.com>
2021-10-09 21:15:01 -0300

    feature: melhorando texto

--- config/router.c
+++ config/router.c
@@ -13,7 +13,7 @@ void router(int *input){
     return show_sobre();
   // rota não encontrada
   else{
-    printf("Nao entendi sua resposta, tente novamente...\n");
+     printf("Nao entendi sua resposta, tente novamente...\n");
     return menu();
   }
 }
