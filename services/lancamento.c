6a7cbc9c4b5483c74721c5a446e3e9236c2b9235
Thiago Braga Martins Gomes <thiago.gomes@pixeon.com>
2021-10-13 10:21:12 -0300

    feat: ajustando metodo para adicionar novo lancamento no model

--- services/lancamento.c
+++ services/lancamento.c
@@ -1,9 +1,3 @@
-// repositório com as informações da carteira
-#define CARTEIRA_FILE "./storage/lancamentos.txt"
-#include <stdio.h>
-#include <time.h>
-#include <assert.h>
-
 char* step_01_mov_types(){
     system("cls");
     printf("Selecione o tipo do lancamento a ser realizado: \n\n");
@@ -25,30 +19,6 @@ char* step_03_descricao(){
     return show_movimentacoes_descricao();
 }
 
-void step_05_final(char *mov_type,long double mov_valor, char *mov_descricao){
-   FILE *data = fopen(CARTEIRA_FILE, "a");
-   fprintf(data, mov_type);
-   fprintf(data, ",");
-
-   char output[50];
-   snprintf(output, 50, "%f", mov_valor);
-   fprintf(data, output);
-   fprintf(data, ",");
-
-    time_t t = time(NULL);
-    struct tm *tm = localtime(&t);
-    char s[64];
-    
-    assert(strftime(s, sizeof(s), "%c", tm));
-    fprintf(data, s);
-    fprintf(data, ",");
-
-   fprintf(data, mov_descricao);
-   fprintf(data, "\n");
-   fclose(data);
-   return ;
-}
-
 void step_04_confirmacao(char *mov_type,long double mov_valor, char *mov_descricao){
     system("cls");
     printf("<< Lancamento a ser realizado >> \n");
@@ -62,7 +32,7 @@ void step_04_confirmacao(char *mov_type,long double mov_valor, char *mov_descric
 
     if(user_input==1){
         system("cls");
-        step_05_final(mov_type, mov_valor,mov_descricao);
+        novo_lancamento(mov_type, mov_valor,mov_descricao);
         printf("Confirmado com sucesso :)\n\n");
         return recursao_menu();
     }else if(user_input==2){
